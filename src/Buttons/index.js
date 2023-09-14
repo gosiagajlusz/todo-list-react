10cc532b4dc0e8e1311b3779a3846696edf6d1f0
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-09-14 15:57:30 +0200

    css styles doubled deleted

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -5,11 +5,11 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
 
   <div className="section__headerButtons">
     {tasks.length > 0 && (<>
-      <button className={`section__headerButtons`}>
+      <button className={`section__headerButtons__button`}>
         {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
       </button>
       <button className=
-        "section__headerButtons"
+        "section__headerButtons__button"
         disabled={tasks.every(({ done }) => done)}
       >
         Ukończ wszystkie
