3ef5322cd9b04f33b88c5c8ad20c485214db7bc0
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-09-15 12:12:50 +0200

    buttons classes updated to BEM convention

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,13 +3,13 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
 
-  <div className="buttons__headerButtons">
+  <div className="buttons">
     {tasks.length > 0 && (<>
-      <button className={"buttons__headerButtons__button"}>
+      <button className={"buttons__button"}>
         {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
       </button>
       <button className=
-        "buttons__headerButtons__button"
+        "buttons__button"
         disabled={tasks.every(({ done }) => done)}
       >
         Ukończ wszystkie
