8e5e31126e2e7813cfd3305be47b8d60770a5f4f
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-10-11 13:35:55 +0200

    created useTasks hook

--- src/App.js
+++ src/App.js
@@ -5,59 +5,21 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useTasks } from "./useTasks";
 
 function App() {
-  const exampleTasks = [
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zrobić obiad", done: true },
-  ];
-
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || exampleTasks
-  );
-
-  const inputRef = useRef(null);
-  const focusInput= ()=> {
-    inputRef.current.focus();
-  }
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
   };
 
-  const toggleTaskDone = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
-        return task;
-      })
-    );
-  };
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
 
-  const setAllDone = () => {
-    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ]);
-  };
-
-  const removeTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  const inputRef = useRef(null);
+  const focusInput = () => {
+    inputRef.current.focus();
   };
 
   return (
@@ -90,6 +52,7 @@ function App() {
             hideDone={hideDone}
             removeTask={removeTask}
             toggleTaskDone={toggleTaskDone}
+            useTasks={useTasks}
           />
         }
       />
