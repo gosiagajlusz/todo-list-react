9ad8af01ce9d7cd27449cdf2bc0ff0cbd90fce7a
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-10-12 18:00:54 +0200

    moved section styling to styled.js

---
+++ src/Section/styled.js
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+
+export const Wrapper = styled.section`
+  background-color: hsl(0, 0%, 100%);
+  margin: 10px;
+  box-shadow: 1px 1px rgb(185, 183, 183);
+`;
+export const Header = styled.header`
+  padding: 20px;
+  border-bottom: 1px solid #ddd;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: auto auto;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 767px) {
+    grid-gap: 10px;
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Title = styled.h2`
+padding: 10px;
+  margin: 0;
+`
+
+export const Body = styled.div`
+//czy to ma sens? trzymam diva dla grida
+`
