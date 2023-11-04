2a604f7080ee57eef2f56b84e1fd98d2f2d3679e
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-04 16:13:56 +0100

    added reducer and redux toolkit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const initialState= {tasks:[],};
+
+const reducer = (state=initialState,action)=>{
+if (action.type==="addTask"){
+  return {
+    ...state,
+    tasks:[]
+  };
+  }
+};
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
