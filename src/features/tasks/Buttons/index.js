d41f95503e1766f154138cd449f01384b7c32b05
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-10 11:38:58 +0100

    changed buttons functions into reducers way

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -4,24 +4,24 @@ import { ButtonsWrapper, Button } from "./styled";
 import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 
 const Buttons = () => {
-  const {tasks,hideDone}= useSelector(selectTasks);
-  const dispatch= useDispatch();
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
   return (
-  <ButtonsWrapper>
-    {tasks.length > 0 && (
-      <>
-        <Button  onClick={()=>dispatch(toggleHideDone())}>
-          {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </Button>
-        <Button
-          disabled={tasks.every(({ done }) => done)}
-          onClick={()=>dispatch(setAllDone())}
-        >
-          Ukończ wszystkie
-        </Button>
-      </>
-    )}
-  </ButtonsWrapper>
-);
+    <ButtonsWrapper>
+      {tasks.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button
+            disabled={tasks.every(({ done }) => done)}
+            onClick={() => dispatch(setAllDone())}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </ButtonsWrapper>
+  );
 };
 export default Buttons;
