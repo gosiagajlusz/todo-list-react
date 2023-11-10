14e069f7a039316a993f7f26b8b0ecb4ee5c5918
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-10 18:48:35 +0100

    added Button Pobierz przykładowe zadania

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,13 +1,19 @@
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { ButtonsWrapper, Button } from "./styled";
-import { selectTasksState, toggleHideDone, setAllDone } from "../tasksSlice";
+import { selectTasksState,
+  toggleHideDone,
+  setAllDone,
+  fetchExampleTasks
+   } from "../tasksSlice";
 
 const Buttons = () => {
   const { tasks, hideDone } = useSelector(selectTasksState);
   const dispatch = useDispatch();
   return (
     <ButtonsWrapper>
+      <Button onClick={()=>dispatch(fetchExampleTasks())}>
+      Pobierz przykładowe zadania</Button>
       {tasks.length > 0 && (
         <>
           <Button onClick={() => dispatch(toggleHideDone())}>
