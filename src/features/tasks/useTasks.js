64ed9e7af8d32235b1d473e42834e756e7b7cca5
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-10 11:38:20 +0100

    commented localData storage to have for future

--- src/features/tasks/useTasks.js
+++ src/features/tasks/useTasks.js
@@ -1,3 +1,5 @@
+//localStorage code for future use
+
 import { useState, useEffect } from "react";
 import { exampleTasks } from "./exampleTasks";
 
@@ -7,55 +9,13 @@ const getInitialState = () => {
 };
 
 export const useTasks = () => {
-  // const [hideDone, setHideDone] = useState(false);
-
-  // const toggleHideDone = () => {
-  //   setHideDone((hideDone) => !hideDone);
-  // };
-
   const [tasks, setTasks] = useState(getInitialState);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const removeTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
-        return task;
-      })
-    );
-  };
-
-  const setAllDone = () => {
-    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-      },
-    ]);
-  };
   return {
     tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-    // hideDone,
-    // setHideDone,
-    // toggleHideDone,
   };
 };
