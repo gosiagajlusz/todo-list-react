707b34cf219417b397ec74d16b630d9a2dfed2cc
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-14 15:01:55 +0100

    added rootSaga

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,17 +1,16 @@
-import {takeEvery,call,put} from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
 import { fetchExampleTasks, setTasks } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 
-export function* fetchExampleTasksHandler(){
-    try {
-        const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
-    } catch (error){
-        yield call(alert, "Coś poszło nie tak")
-    }
+export function* fetchExampleTasksHandler() {
+  try {
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  } catch (error) {
+    yield call(alert, "Coś poszło nie tak");
+  }
 }
 
 export function* watchFetchExampleTasks() {
-    console.log("Saga działa! :)")
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler)
+  yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
 }
