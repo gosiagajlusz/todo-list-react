9af813eaed9b18890c663fe6780fcd4065bbf056
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-14 11:22:36 +0100

    added saga

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -2,15 +2,16 @@ import {takeEvery,call,put} from "redux-saga/effects";
 import { fetchExampleTasks, setTasks } from "./tasksSlice";
 import { getExampleTasks } from "./getExampleTasks";
 
-function* fetchExampleTasksHandler(){
+export function* fetchExampleTasksHandler(){
     try {
         const exampleTasks = yield call(getExampleTasks);
         yield put(setTasks(exampleTasks));
     } catch (error){
-        yield call(alery, "Coś poszło nie tak")
+        yield call(alert, "Coś poszło nie tak")
     }
 }
 
-function* watchFetchExampleTasks() {
+export function* watchFetchExampleTasks() {
+    console.log("Saga działa! :)")
     yield takeEvery(fetchExampleTasks.type)
 }
