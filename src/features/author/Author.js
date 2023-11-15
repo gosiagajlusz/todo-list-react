5f40d250e22c1cd3e3190662af5bbf57da028208
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-15 16:39:55 +0100

    added Author page

---
+++ src/features/author/Author.js
@@ -0,0 +1,25 @@
+import Container from "../../common/Container"
+import Section from "../../common/Section"
+import Header from "../../common/Header"
+import { theme } from "../../theme"
+import { GlobalStyle } from "../../globalStyles"
+import { ThemeProvider } from "styled-components";
+
+
+export const Author = ()=>(
+    <ThemeProvider theme={theme}>
+    <Container>
+        <Header title="O autorze"/>
+        <Section
+        title= "Małgorzata Gajlusz"
+        body={
+            <>
+            <p>Jestem Gosia. Projektuję wnętrza i uczę się tworzenia
+            aplikacji i stron
+            internetowych.Dążę do tego, żeby aplikacje
+             mojego autorstwa były estetyczne i przyjemne w użytkowaniu.</p></>
+        }
+        />
+    </Container>
+    </ThemeProvider>
+)
--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,6 @@ export const theme = {
   },
 
   breakpoints: {
-    mobileMax: 767,
+    mobile: 767,
   },
 };
