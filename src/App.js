e0c0735e5ea127ee00382ee6a98ec409b6f08364
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-15 16:38:58 +0100

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,2 +1,31 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks"
-export const App = ()=><Tasks/>
+import { Author } from "./features/author/Author";
+
+
+
+export const App = ()=>(
+    <BrowserRouter>
+    <nav>
+        <ul>
+            <li>
+                <Link to="/zadania">
+                Zadania
+                </Link>
+            </li>
+            <li>
+                <Link to="/autor">
+                O autorze
+                </Link>
+            </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                <Tasks/>
+                </Route>
+                <Route path="/autor">
+                <Author/>
+                </Route>
+            </Switch>
+    </nav>
+    </BrowserRouter>);
