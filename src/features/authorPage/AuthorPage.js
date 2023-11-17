94884a5e4ad7b7a415041bed92a0a69c9d275d3a
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 19:38:12 +0100

    styled paragraph

--- src/features/authorPage/AuthorPage.js
+++ src/features/authorPage/AuthorPage.js
@@ -2,8 +2,8 @@ import Container from "../../common/Container"
 import Section from "../../common/Section"
 import Header from "../../common/Header"
 import { theme } from "../../theme"
-import { GlobalStyle } from "../../globalStyles"
 import { ThemeProvider } from "styled-components";
+import { Paragraph } from "./styled";
 
 
 export const AuthorPage = ()=>(
@@ -14,10 +14,11 @@ export const AuthorPage = ()=>(
         title= "Małgorzata Gajlusz"
         body={
             <>
-            <p>Jestem Gosia. Projektuję wnętrza i uczę się tworzenia
+            <Paragraph>Jestem Gosia. Projektuję wnętrza i uczę się tworzenia
             aplikacji i stron
-            internetowych.Dążę do tego, żeby aplikacje
-             mojego autorstwa były estetyczne i przyjemne w użytkowaniu.</p></>
+            internetowych.<br>
+            </br>Dążę do tego, żeby aplikacje
+             mojego autorstwa były estetyczne i przyjemne w użytkowaniu.</Paragraph></>
         }
         />
     </Container>
