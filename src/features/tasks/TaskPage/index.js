7dcb1fc4967e746de02c39be6ac09498157e1731
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 17:17:55 +0100

    added task page

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,22 +1,26 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Container from "../../../common/Container";
 import { theme } from "../../../theme";
 import { GlobalStyle } from "../../../globalStyles";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
         <Header title="Szczegóły zadania" />
-        <Section title="test"
-         body={<>xd</>} />
-        
+        <Section
+          title={task ? task.content : "Niestety nie znaleziono zadania 😿"}
+          body={<>{task ? `Ukończone: ${task.done ? "Tak" : "Nie"}` : ""}</>}
+        />
       </Container>
     </ThemeProvider>
   );
