9ac1c48888aa54bff2f38514920f3bd1247aee30
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 17:13:27 +0100

    added navigation

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage"
 import TaskPage from "./features/tasks/TaskPage"
 import { AuthorPage } from "./features/authorPage/AuthorPage";
+import { StyledNavLink } from "./styled";
+import { NavList } from "./styled";
 
 
 export const App = ()=>(
     <HashRouter>
     <nav>
-        <ul>
+        <NavList>
             <li>
-                <Link to="/zadania">
+                <StyledNavLink to="/zadania">
                 Zadania
-                </Link>
+                </StyledNavLink>
             </li>
             <li>
                 <Link to="/autor">
                 O autorze
                 </Link>
             </li>
-            </ul>
+            </NavList>
             <Switch>
-                <Route path="/zadania">
-                    <TasksPage></TasksPage>
-                </Route>
                 <Route path="/zadania/:id">
-                <TaskPage/>
+                    <TaskPage/>
+                </Route>
+                <Route path="/zadania">
+                <TasksPage/>
                 </Route>
                 <Route path="/autor">
                 <AuthorPage/>
