bd4397bb20b4ae6ab3f00926ff89b8a9e18f7c37
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 17:14:26 +0100

    added HideDone state to localstorage

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -1,8 +1,16 @@
  
  const localStorageKey = "tasks";
+ const localStorageHideDoneKey = "hideDone";
+
  
  export const saveTasksInLocalStorage = tasks =>
  localStorage.setItem(localStorageKey,JSON.stringify(tasks));
 
  export const getTasksFromLocalStorage = ()=>
  JSON.parse(localStorage.getItem(localStorageKey)) || [];
+
+ export const saveHideDoneStateInLocalStorage = (hideDone) =>
+ localStorage.setItem(localStorageHideDoneKey, JSON.stringify(hideDone));
+
+export const getHideDoneStateFromLocalStorage = () =>
+ JSON.parse(localStorage.getItem(localStorageHideDoneKey)) || false;
