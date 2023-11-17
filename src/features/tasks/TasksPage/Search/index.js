2e94232d76fd2c97b1211fcdb9e3275553014c49
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 17:19:30 +0100

    created search component

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,31 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min"
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Input from "../../Input";
+import { InputWrapper } from "./styled";
+import searchQueryParamName from "../TasksList/searchQueryParamName";
+
+export const Search = ()=> {
+const location = useLocation();
+const history = useHistory();
+const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+
+const onInputChange = ({target})=> {
+const searchParams = new URLSearchParams(location.search);
+
+if (target.value.trim()===""){
+    searchParams.delete(searchQueryParamName);
+}else {
+    searchParams.set(searchQueryParamName,target.value);
+}
+history.push(`${location.pathname}?${searchParams.toString()}`);
+};
+
+    return (
+        <InputWrapper>
+        <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+        /></InputWrapper>
+    )
+}
