1ae11f9547aaf8e9c1c42ce84bb534576bbcf9cb
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-17 17:19:08 +0100

    added search and commented tasks use Selector

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import Form from "./Form";
-import TasksList from "./TasksList"
+import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
@@ -10,16 +10,20 @@ import Container from "../../../common/Container";
 import { theme } from "../../../theme";
 import { GlobalStyle } from "../../../globalStyles";
 import { selectTasksState } from "../tasksSlice";
+import { Search } from "./Search";
 
 function TasksPage() {
-  const { tasks } = useSelector(selectTasksState);
+  // const { tasks } = useSelector(selectTasksState);
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
         <Header title="Lista zadań" />
+
         <Section title="Dodaj nowe zadanie" body={<Form />} />
+        <Section title="wyszukiwarka" body={<Search />} />
+
         <Section
           title="Lista zadań"
           extraHeaderContent={<Buttons />}
