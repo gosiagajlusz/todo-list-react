a39ce0c0b3b5b66f7e67ac981f1430ae84fd4015
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-18 19:13:19 +0100

    added saveHideDone state

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -1,16 +1,14 @@
- 
- const localStorageKey = "tasks";
- const localStorageHideDoneKey = "hideDone";
+const localStorageKey = "tasks";
+const localStorageHideDoneKey = "hideDone";
 
- 
- export const saveTasksInLocalStorage = tasks =>
- localStorage.setItem(localStorageKey,JSON.stringify(tasks));
+export const saveTasksInLocalStorage = (tasks) =>
+  localStorage.setItem(localStorageKey, JSON.stringify(tasks));
 
- export const getTasksFromLocalStorage = ()=>
- JSON.parse(localStorage.getItem(localStorageKey)) || [];
+export const getTasksFromLocalStorage = () =>
+  JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
- export const saveHideDoneStateInLocalStorage = (hideDone) =>
- localStorage.setItem(localStorageHideDoneKey, JSON.stringify(hideDone));
+export const saveHideDoneStateInLocalStorage = (hideDone) =>
+  localStorage.setItem(localStorageHideDoneKey, JSON.stringify(hideDone));
 
 export const getHideDoneStateFromLocalStorage = () =>
- JSON.parse(localStorage.getItem(localStorageHideDoneKey)) || false;
+  JSON.parse(localStorage.getItem(localStorageHideDoneKey)) || false;
--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,7 @@
 import { all } from "redux-saga/effects";
 
 import { tasksSaga } from "./features/tasks/tasksSaga";
- 
-export default function * rootSaga(){
-    yield all ([
-        tasksSaga(),
-    ]);
+
+export default function* rootSaga() {
+  yield all([tasksSaga()]);
 }
