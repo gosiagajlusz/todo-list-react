7381ec2d7d2643b1b3c20bd1ccff88a2b8ffcdb8
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2023-11-20 14:25:28 +0100

    AuthorPage formated and styled

--- src/features/authorPage/AuthorPage.js
+++ src/features/authorPage/AuthorPage.js
@@ -1,30 +1,28 @@
-import Container from "../../common/Container"
-import Section from "../../common/Section"
-import Header from "../../common/Header"
-import { theme } from "../../theme"
+import Container from "../../common/Container";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import { theme } from "../../theme";
 import { ThemeProvider } from "styled-components";
 import { Paragraph } from "./styled";
 
-
-export const AuthorPage = ()=>(
-    <ThemeProvider theme={theme}>
+export const AuthorPage = () => (
+  <ThemeProvider theme={theme}>
     <Container>
-        <Header title="O autorze"/>
-        <Section
-        title= "Małgorzata Gajlusz"
+      <Header title="O autorze" />
+      <Section
+        title="Małgorzata Gajlusz"
         body={
-            <>
-            <Paragraph>Jestem Gosia. Projektuję wnętrza i uczę się tworzenia
-            aplikacji i stron
-            internetowych.<br>
-            </br>Dążę do tego, żeby aplikacje
-             mojego autorstwa były estetyczne i przyjemne w użytkowaniu.
-             <br>
-             </br>
-             Moje pasje to gotowanie, fitness i architektura wnętrz.
-             </Paragraph></>
+          <>
+            <Paragraph>
+              Jestem Gosia. Projektuję wnętrza i uczę się tworzenia aplikacji i
+              stron internetowych.<br></br>Dążę do tego, żeby aplikacje mojego
+              autorstwa były estetyczne i przyjemne w użytkowaniu.
+              <br></br>
+              Moje pasje to gotowanie, fitness i architektura wnętrz.
+            </Paragraph>
+          </>
         }
-        />
+      />
     </Container>
-    </ThemeProvider>
-)
+  </ThemeProvider>
+);
--- src/features/authorPage/styled.js
+++ src/features/authorPage/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const Paragraph = styled.p`
-text-align: justify;
-padding:20px;
-`
+  text-align: justify;
+  padding: 15px 15px 25px 15px;
+`;
